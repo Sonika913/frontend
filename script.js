c480fd40e3294cb763307a4a6ec41cb6e4976b3b
sonika <sonikastha34@gmail.com>
2025-05-06 07:48:17 +0545

    inbc

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ const navlinks = document.getElementById('navlinks');
 hamburger.addEventListener('click',()=>{
     navlinks.classList.toggle('active');
 });
+
+const text = "Hi, I'm miss. S";
+const hii = document.getElementById('hii');
+let index = 0;
+
+function type() {
+    if(index < text.length){
+        hii.textContent += text.charAt(index);
+        index++;
+        setTimeout(type, 150);
+    }
+}
+type();

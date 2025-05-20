16dd7bad777086ea2b81fa22f03fdc4e616f9340
sonika <sonikastha34@gmail.com>
2025-05-20 10:03:58 +0545

    ghf

--- script.js
+++ script.js
@@ -23,16 +23,19 @@ const projects = [
      image: "image/flower.jpg",
      title: "Animated Navbar",
      description: "A responsive navigation bar with smooth animations and toogle functionality. ",
+     src: " https://sonika913.github.io/bmi-calculator/",
     },
     {
      image: "image/fl.jpg ",
      title: "Landing Page ",
      description: "A clean and simple landing page using HTML, CSS and basic JS.",
+    src: " https://sonika913.github.io/bmi-calculator/",
     },
     {
      image: "image/pexels-aliye-2151376458-31673403.jpg",
      title: "CSS",
      description: "CSS describes how HTML elements are to be displayed on screen, paper, or in other media CSS saves a lot of work.",
+     src: " https://sonika913.github.io/bmi-calculator/",
     },
 ];
 
@@ -45,6 +48,7 @@ const container = document.getElementById("project-container");
           <img src="${project.image}" alt="${project.title}" \>
           <h3>${project.title}</h3>
           <p>${project.description}</p>
+          <a href="${project.src}">view project</a>
         `;
         container.appendChild(card);
     });
